e79952a90fe25a0472033acc02d6159d8665f198
Fred Willien <frederic.willien@gmail.com>
2020-09-17 15:08:15 +0200

    commit

--- travis/build.sh
+++ travis/build.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 set -x
 
 SERVICE_NAME=$1
 AWS_ECR_URI=$2
 TAG=$3
 
-if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]
-then
+#if [[ "${TRAVIS_PULL_REQUEST}" = "false" ]]
+#then
 
-  if [[ "${TRAVIS_BRANCH}" =^ '^(develop|master)$']]; then
-    IMAGE_TAG=${TRAVIS_BRANCH}
-  else
-    IMAGE_TAG=${TAG}
-  fi
+  IMAGE_TAG="${TAG:-$TRAVIS_BRANCH}"
   docker build -f ${TRAVIS_BUILD_DIR}/docker/phenix/Dockerfile --no-cache --tag ${SERVICE_NAME} .
   docker tag ${SERVICE_NAME}:latest ${AWS_ECR_URI}/${SERVICE_NAME}:${IMAGE_TAG}
   docker push ${AWS_ECR_URI}/${SERVICE_NAME}:${IMAGE_TAG}
@@ -24,4 +20,4 @@ then
     docker tag ${SERVICE_NAME}:latest ${AWS_ECR_URI}/${SERVICE_NAME}:latest
     docker push ${AWS_ECR_URI}/${SERVICE_NAME}:latest
   fi
-fi
+#fi
